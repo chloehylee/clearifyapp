1ebe659983167d3d7061fee22970741a329065ff
chloehylee <335732582@gapps.yrdsb.ca>
2023-08-07 19:49:29 -0400

    Initial Commit

---
+++ Clearify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clearify
+//
+//  Created by Chloe Lee on 2023-08-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClearifyUITests/ClearifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ClearifyUITestsLaunchTests.swift
+//  ClearifyUITests
+//
+//  Created by Chloe Lee on 2023-08-07.
+//
+
+import XCTest
+
+final class ClearifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
